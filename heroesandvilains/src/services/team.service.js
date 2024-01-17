52ef29a015f06d48792130fdbfcd93c6f7e3b4fd
Mathys <mathys.nourry@edu.univ-fcomte.fr>
2024-01-17 15:58:01 +0100

    Team service

--- heroesandvilains/src/services/team.service.js
+++ heroesandvilains/src/services/team.service.js
@@ -1,1 +1,50 @@
 import Axios from "./axios.service";
+
+let getTeams = async () => {
+    try {
+       const res = await Axios.get('/teams/get');
+       return res.data;
+    } catch(err) {
+        throw new Error(err.response.data);
+    }
+}
+
+let createTeam = async (name) => {
+    try {
+        const res = await Axios.post('/teams/create', name);
+        return res.data;
+    } catch(err) {
+        throw new Error(err.response.data);
+    }
+}
+
+let addHeroes = async (idHeroes, idTeam) => {
+    try {
+        const res = await Axios.patch('/teams/addheroes', {
+            idHeroes,
+            idTeam
+        });
+        return res.data;
+    } catch(err) {
+        throw new Error(err.response.data);
+    }
+}
+
+let removeHeroes = async (idHeroes, idTeam) => {
+    try {
+        const res = await Axios.patch('/teams/removeheroes', {
+            idHeroes,
+            idTeam
+        });
+        return res.data;
+    } catch(err) {
+        throw new Error(err.response.data);
+    }
+}
+
+module.exports = {
+    getTeams,
+    createTeam,
+    addHeroes,
+    removeHeroes
+}
