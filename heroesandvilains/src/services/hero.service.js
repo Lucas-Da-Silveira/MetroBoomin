700b8b0abd004d64a4f18364696d1c1ab6f0b3de
Lucas Da Silveira <lucasdslvra@gmail.com>
2024-01-17 18:25:33 +0100

    hero.service.js fini

--- heroesandvilains/src/services/hero.service.js
+++ heroesandvilains/src/services/hero.service.js
@@ -1,1 +1,52 @@
 import Axios from "./axios.service";
+
+let getHeroAliases = async () => {
+    try {
+        const res = await Axios.get('/heroes/getaliases');
+        return res.data;
+    } catch (err) {
+        throw new Error(err.response.data);
+    }
+}
+
+let createHero = async (heroData) => {
+    try {
+        const res = await Axios.post('/heroes/create', heroData);
+        return res.data;
+    } catch (err) {
+        throw new Error(err.response.data);
+    }
+}
+
+let updateHero = async (heroData, orgSecret) => {
+    try {
+        const res = await Axios.put('/heroes/update', heroData, {
+            headers: {
+                'org-secret': orgSecret
+            }
+        });
+        return res.data;
+    } catch (err) {
+        throw new Error(err.response.data);
+    }
+}
+
+let getHeroById = async (heroId, orgSecret) => {
+    try {
+        const res = await Axios.get(`/heroes/getbyid/${heroId}`, {
+            headers: {
+                'org-secret': orgSecret
+            }
+        });
+        return res.data;
+    } catch (err) {
+        throw new Error(err.response.data);
+    }
+}
+
+module.exports = {
+    getHeroAliases,
+    createHero,
+    updateHero,
+    getHeroById
+}
