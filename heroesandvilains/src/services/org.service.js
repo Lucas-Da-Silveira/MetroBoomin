d6eceece7bb1bf7a511cc58a4b0381369b6a83c4
Mathys <mathys.nourry@edu.univ-fcomte.fr>
2024-01-17 16:37:55 +0100

    Made org.service.js

--- heroesandvilains/src/services/org.service.js
+++ heroesandvilains/src/services/org.service.js
@@ -1,1 +1,69 @@
 import Axios from "./axios.service";
+
+let getOrgs = async () => {
+    try {
+        const res = await Axios.get('/orgs/get');
+        return res.data;
+    } catch(err) {
+        throw new Error(err.response.data);
+    }
+}
+
+let createOrg = async (name, secret) => {
+    try {
+        const res = await Axios.post('/orgs/create', {
+            name,
+            secret
+        });
+        return res.data;
+    } catch(err) {
+        throw new Error(err.response.data);
+    }
+}
+
+let addTeam = async (idTeam, secret) => {
+    try {
+        const res = await Axios.patch('/orgs/addteam', idTeam, {
+            headers: {
+                'org-secret': secret
+            }
+        });
+        return res.data;
+    } catch(err) {
+        throw new Error(err.response.data);
+    }
+}
+
+let removeTeam = async (idTeam, secret) => {
+    try {
+        const res = await Axios.patch('/orgs/removeteam', idTeam, {
+            headers: {
+                'org-secret': secret
+            }
+        });
+        return res.data;
+    } catch(err) {
+        throw new Error(err.response.data);
+    }
+}
+
+let getOrgById = async (idOrg, secret) => {
+    try {
+        const res = await Axios.get(`/orgs/getbyid/${idOrg}`, {
+            headers: {
+                'org-secret': secret
+            }
+        });
+        return res.data;
+    } catch(err) {
+        throw new Error(err.response.data);
+    }
+}
+
+module.exports = {
+    getOrgs,
+    createOrg,
+    addTeam,
+    removeTeam,
+    getOrgById
+}
