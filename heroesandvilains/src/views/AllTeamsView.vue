22a912b29f7e5e3ee6597ab7e2e6ec5c4ae63276
Lucas Da Silveira <lucasdslvra@gmail.com>
2024-01-21 16:29:15 +0100

    Création équipe fonctionnelle

--- heroesandvilains/src/views/AllTeamsView.vue
+++ heroesandvilains/src/views/AllTeamsView.vue
@@ -9,11 +9,9 @@ export default {
     return {
       headers: [
         {text: 'Team Name', align: 'start', sortable: true, value: 'name'},
-        // Add other headers as needed based on your team data structure
       ],
       dialogVisible: false,
       newTeamName: '',
-      // Add other properties for new team creation as needed
     };
   },
   methods: {
@@ -23,24 +21,19 @@ export default {
     },
     closeTeamCreationDialog() {
       this.dialogVisible = false;
-      // Reset any form fields if needed
       this.newTeamName = '';
     },
     createNewTeam() {
-      // Add logic to create a new team and update the list
-      this.createTeam({name: this.newTeamName /* Add other properties */})
+      this.createTeam({name: this.newTeamName })
           .then(() => {
-            // Close the dialog and reset form fields
             this.closeTeamCreationDialog();
           })
           .catch((error) => {
             console.error('Error creating team:', error.message);
-            // Handle error if needed
           });
     },
   },
   created() {
-    // Load teams when the component is created
     this.loadTeams();
   },
 };
@@ -55,19 +48,14 @@ export default {
         class="elevation-1"
     ></v-data-table>
 
-    <!-- Button to trigger team creation -->
     <v-btn @click="openTeamCreationDialog">Create New Team</v-btn>
 
-    <!-- Dialog for creating a new team -->
     <v-dialog v-model="dialogVisible" max-width="600">
       <v-card>
         <v-card-title>Create New Team</v-card-title>
         <v-card-text>
-          <!-- Your form for creating a new team goes here -->
-          <!-- Example: -->
           <v-form @submit.prevent="createNewTeam">
             <v-text-field v-model="newTeamName" label="Team Name"></v-text-field>
-            <!-- Add other form fields as needed -->
             <v-btn type="submit">Create</v-btn>
           </v-form>
         </v-card-text>
