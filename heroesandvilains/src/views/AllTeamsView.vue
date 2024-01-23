f3e3297d59ffc22ca795c695b82f1ff0c46b309a
Lucas Da Silveira <lucasdslvra@gmail.com>
2024-01-23 14:44:44 +0100

    Style team

--- heroesandvilains/src/views/AllTeamsView.vue
+++ heroesandvilains/src/views/AllTeamsView.vue
@@ -40,15 +40,23 @@ export default {
 </script>
 
 <template>
-  <div>
+ <v-container class="container">
     <v-data-table
         :headers="headers"
         :items="teams"
         :items-per-page="5"
         class="elevation-1"
-    ></v-data-table>
+    >
 
-    <v-btn @click="openTeamCreationDialog">Create New Team</v-btn>
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Teams</v-toolbar-title>
+          <v-spacer/>
+          <v-btn id="open-create-dialog" color="primary" @click="openTeamCreationDialog">NEW TEAM</v-btn>
+        </v-toolbar>
+      </template>
+
+    </v-data-table>
 
     <v-dialog v-model="dialogVisible" max-width="600">
       <v-card>
@@ -56,17 +64,19 @@ export default {
         <v-card-text>
           <v-form @submit.prevent="createNewTeam">
             <v-text-field v-model="newTeamName" label="Team Name"></v-text-field>
-            <v-btn type="submit">Create</v-btn>
+            <v-btn color="green" type="submit">Create</v-btn>
           </v-form>
         </v-card-text>
         <v-card-actions>
-          <v-btn @click="closeTeamCreationDialog">Cancel</v-btn>
+          <v-btn color="red" @click="closeTeamCreationDialog">Cancel</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
-  </div>
+  </v-container>
 </template>
 
 <style scoped>
-
+.container {
+  width: 60%;
+}
 </style>
