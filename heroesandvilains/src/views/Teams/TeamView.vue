cb1127f0db9ccbf4a8898456626b038894eebca9
Mathys <mathys.nourry@edu.univ-fcomte.fr>
2024-04-01 15:42:25 +0200

    Minor fixes

--- heroesandvilains/src/views/Teams/TeamView.vue
+++ heroesandvilains/src/views/Teams/TeamView.vue
@@ -33,7 +33,7 @@ export default {
     ...mapMutations(['setCurrentTeam']),
 
     async seeHeroInfo(hero) {
-      this.loadHeroDetails(hero._id);
+      await this.loadHeroDetails(hero._id);
       await this.$router.push(`/heroes/${hero._id}`);
     },
 
